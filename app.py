99e365ef84935691779fefae482cd158f7a0144b
Frankie Vexx <franblog7@gmail.com>
2023-09-07 22:12:28 +0300

    updating flask app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json, time, datetime
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def APIend():
     # Get slack name and track from the url
     slack_name = request.args.get('slack_name')
@@ -15,20 +15,23 @@ def APIend():
     
     # Github information
     github_repo_url = "https://github.com/FrankieVexx/Zuri_Internship"
-    github_file_url = f'{github_repo_url}/apiend.py'
+    github_file_url = f'{github_repo_url}/app.py'
     
     
-    data = {"slack_name": "FrankieVexx",
+    data = {"slack_name": "Frankie",
         "current_day": "Thursday",
-        "utc_time": "2023-09-07T09:33:05Z",
+        "utc_time": "2023-09-07T21:55:45Z",
         "track": "backend",
-        "github_file_url": "https://github.com/FrankieVexx/Zuri_Internship/apiend.py",
+        "github_file_url": "https://github.com/FrankieVexx/Zuri_Internship/app.py",
         "github_repo_url": "https://github.com/FrankieVexx/Zuri_Internship",
          "status_code": "200"
     }
+    
+    data["current_day"] = current_day
+    data["utc_time"] = current_time
     
     return jsonify(data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8000)
